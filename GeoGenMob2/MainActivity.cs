4312c0142fde650f3741a5b077aa40159a206e0a
pmn3gm@gmail.com <pmn3gm@gmail.com>
2019-08-18 23:57:53 +0300

    +

--- GeoGenMob2/MainActivity.cs
+++ GeoGenMob2/MainActivity.cs
@@ -4,6 +4,9 @@ using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
 
+using Geocore;
+
+
 namespace GeoGenMob2
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
@@ -15,6 +18,41 @@ namespace GeoGenMob2
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            //==========
+            EditText urltext = FindViewById<EditText>(Resource.Id.textInputEditText1);
+            EditText usertext = FindViewById<EditText>(Resource.Id.textInputEditText2);
+            EditText devtext = FindViewById<EditText>(Resource.Id.textInputEditText3);
+            EditText countertext = FindViewById<EditText>(Resource.Id.textInputEditText4);
+            TextView printtext = FindViewById<TextView>(Resource.Id.textView5);
+            TextView printmess = FindViewById<TextView>(Resource.Id.textView6);
+
+
+            Button savebutton = FindViewById<Button>(Resource.Id.button1);
+            Button startbutton = FindViewById<Button>(Resource.Id.button2);
+            //==========
+            savebutton.Click += (sender, e) =>
+            {
+                //string url = urltext.Text;
+                //printtext.SetText();
+                Geocore.Settings.SaveJSON(urltext.Text,usertext.Text,devtext.Text);
+                printtext.Text = "url: "+urltext.Text+"\n"+"user: "+usertext.Text+"\n"+"dev: "+devtext.Text;                
+            };
+
+            startbutton.Click += (sender, e) =>
+            {
+                // int n0 = int.Parse(countertext.Text);
+                //countertext.Text = "2";
+                int n0;
+                if (int.TryParse(countertext.Text,out n0))
+                {
+                    Geocore.Settings.startSend(n0);
+                }
+                else
+                {
+                    printmess.Text = "В поле counter укажите число."; 
+                }
+               
+            };
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
--- GeoGenMob2/geocore.cs
+++ GeoGenMob2/geocore.cs
@@ -1,1 +1,154 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace Geocore
+{
+   // public static class Settings
+   public class Settings
+    {
+        class GEO
+        {
+            public string url0;
+            public string nameID;
+            public string geonamedevice;
+            public double X;
+            public double Y;
+        }
+
+        //SaveJSON  - Сохраняем настройки в файл JSON (Сереализация)
+        public static void SaveJSON(string url1, string user1, string dev1)
+      // public async Task SaveJSON(string url1, string user1, string dev1)
+        {
+            GEO initgeo = new GEO();
+            initgeo.url0 = url1;
+            initgeo.nameID = user1;
+            initgeo.geonamedevice = dev1;
+
+            string initjson = JsonConvert.SerializeObject(initgeo);
+
+            //string path = @"settings.conf";
+            // тут происходит неведомая хрень по выделению места под файл geosetting.conf
+            var geogenmobpath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal),"geosettings.conf");
+            using (var conf = File.CreateText(geogenmobpath))
+            {
+               conf.WriteLine(initjson);
+            }
+        }
+
+        //Читаем файл JSON и возвращаем его содержимое ввиде строки
+        private static string printJSON()
+        //public async Task<string> printJSON()
+        {
+            string urlline = "";
+            //string path = @"settings.conf";
+            string line = "";
+
+            var geogenmobpath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "geosettings.conf");
+            if (geogenmobpath == null || !File.Exists(geogenmobpath))
+            {
+                return "File not found";
+            }
+            using (var conf = File.OpenText(geogenmobpath))
+            {
+                while (line != null)
+                {
+                    line = conf.ReadLine();
+                    //Console.WriteLine(line);
+                    if (line != null)
+                    {
+                        urlline = line;
+                    }
+                }
+            }
+            return urlline;
+        }
+
+        //Десериализуем полученную строку из файла JSON и
+        // отправляем на сервер
+
+        public static void SendGEO(object n)
+        //public static async void SendGEO(object n)
+        {
+            Random rnd = new Random();
+            //string pathlog = @"sendgeo.log";
+
+            //string url = "https://localhost:44359/home/inputgeoJSON";
+            //string url = "http://random-red.ddns.net:62424/home/inputgeoJSON";
+            string jsonstr = printJSON();
+            GEO testgeo = JsonConvert.DeserializeObject<GEO>(jsonstr);
+
+            int n1 = int.Parse(n.ToString()); //перобразовываем из n - object в n1 int 
+
+            try
+            {
+                for (int i = 0; i < n1; i++)
+                {
+
+                    testgeo.X = rnd.Next(99);
+
+                    testgeo.Y = rnd.Next(99);
+
+                    string json = JsonConvert.SerializeObject(testgeo);
+                    Console.WriteLine("JSON: {0}", json);
+
+                    var httpRequest = (HttpWebRequest)WebRequest.Create(testgeo.url0);
+                    httpRequest.Method = "POST";
+                    httpRequest.ContentType = "application/json";
+                    using (var requestStream = httpRequest.GetRequestStream())
+                    using (var writer = new StreamWriter(requestStream))
+                    {
+                        writer.Write(json);
+                    }
+                    using (var httpResponse = httpRequest.GetResponse())
+                    using (var responseStream = httpResponse.GetResponseStream())
+                    using (var reader = new StreamReader(responseStream))
+                    {
+                       string response = reader.ReadToEnd();
+                    }
+
+                }
+            }
+            catch (WebException e)
+            {
+                var geogenlogpath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "geogen.log");
+                using (var log = File.CreateText(geogenlogpath))
+                {
+                    log.WriteLine(e);
+                }
+
+                //using (StreamWriter log = File.CreateText(pathlog))
+                //{
+                //    log.WriteLine(e);
+                //    log.WriteLine("-----");
+                //}
+                // Console.WriteLine(e); //показываем ошибку
+                //Console.WriteLine("");
+            }
+            //Console.WriteLine("===>OK<===");
+        }
+
+        //Создаём поток SendGEO
+        public static void startSend(int n0)
+        {            
+            Thread ThreadGenGC = new Thread(SendGEO);
+            ThreadGenGC.Start(n0);
+               // Console.WriteLine("Поток ThreadGenGC запущен");
+            ThreadGenGC.Join();
+            //Console.WriteLine("Повторить? (да - 1,нет - 0)");
+            //return "Отправка завершена.";
+
+        }
+
+
+    }
+}
